cb51877d01a73cffb5897af9efcc39835d58d0ce
Mohamed Jalal <technochtak3@gmail.com>
2024-03-12 02:32:35 +0000

    main

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -6,32 +6,32 @@ const TopNavbar = () => {
     
   return (
 
-        <div className=" w-full grid grid-cols-5 h-12 items-center justify-center gap-2 py-2">
-            <div className='group flex w-full h-full rounded-lg items-center justify-center text-lg font-meduim cursor-pointer'>
+        <div className=" w-full grid grid-cols-5 h-12 items-center justify-center gap-2 py-2 text-md 2xl:text-lg lg:text-lg">
+            <div className='group flex w-full h-full rounded-lg items-center justify-center  font-semibold cursor-pointer'>
                 <div className='relative py-1'>
                     Home
                     <div className=' group-active:scale-x-100 group-active:opacity-100 transition-all duration-200 ease-in-out scale-x-0 group-hover:scale-x-100 group-hover:opacity-100 opacity-0 w-full absolute bottom-0 left-0 h-1.5 rounded-lg bg-gradient-to-r from-green-500 via-emerald-300 to-green-600'/>
                 </div>
             </div>
-            <div className='group flex w-full h-full rounded-lg items-center justify-center text-lg font-meduim cursor-pointer'>
+            <div className='group flex w-full h-full rounded-lg items-center justify-center  font-semibold cursor-pointer'>
                 <div className='relative py-1'>
                     Work
                     <div className=' group-active:scale-x-100 group-active:opacity-100 transition-all duration-200 ease-in-out scale-x-0 group-hover:scale-x-100 group-hover:opacity-100 opacity-0 w-full absolute bottom-0 left-0 h-1.5 rounded-lg bg-gradient-to-r from-green-500 via-emerald-300 to-green-600'/>
                 </div>
             </div>
-            <div className='group flex w-full h-full rounded-lg items-center justify-center text-3xl font-bold cursor-pointer'>
+            <div className='group flex w-full h-full rounded-lg items-center justify-center text-2xl 2xl:text-3xl lg:text-3xl font-bold cursor-pointer'>
                 <div className='relative pb-1'>
                     JDev
                     <div className=' group-active:scale-x-100 group-active:opacity-100 transition-all duration-200 ease-in-out scale-x-0 group-hover:scale-x-100 group-hover:opacity-100 opacity-0 w-full absolute bottom-0 left-0 h-1.5 rounded-lg bg-gradient-to-r from-green-500 via-emerald-300 to-green-600'/>
                 </div>
             </div>
-            <div className='group flex w-full h-full rounded-lg items-center justify-center text-lg font-meduim cursor-pointer'>
+            <div className='group flex w-full h-full rounded-lg items-center justify-center  font-semibold cursor-pointer'>
                 <div className='relative py-1'>
                     Hire me
                     <div className=' group-active:scale-x-100 group-active:opacity-100 transition-all duration-200 ease-in-out scale-x-0 group-hover:scale-x-100 group-hover:opacity-100 opacity-0 w-full absolute bottom-0 left-0 h-1.5 rounded-lg bg-gradient-to-r from-green-500 via-emerald-300 to-green-600'/>
                 </div>
             </div>
-            <div className='group flex w-full h-full rounded-lg items-center justify-center text-lg font-meduim cursor-pointer'>
+            <div className='group flex w-full h-full rounded-lg items-center justify-center font-semibold cursor-pointer'>
                 <div className='relative py-1'>
                     Contact
                     <div className=' group-active:scale-x-100 group-active:opacity-100 transition-all duration-200 ease-in-out scale-x-0 group-hover:scale-x-100 group-hover:opacity-100 opacity-0 w-full absolute bottom-0 left-0 h-1.5 rounded-lg bg-gradient-to-r from-green-500 via-emerald-300 to-green-600'/>
