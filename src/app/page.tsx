f6ac62cc6cf4cc26f76d978c11fb666b2f50051a
Mohamed Jalal <technochtak3@gmail.com>
2024-03-12 03:12:08 +0000

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col w-full">
+    <main className="flex min-h-screen flex-col w-full bg-zinc-100">
       <MaxWidthWrapper>
         <TopNavbar />
         <HeroSection />
