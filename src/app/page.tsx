ebcb9adf46a4263f4e50809bd76c450132e93c3e
Mohamed Jalal <technochtak3@gmail.com>
2024-03-15 20:22:17 +0000

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,21 @@ import SliderShow from "@/components/SliderShow";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col w-full bg-zinc-100 snap-y snap-proximity snap-always">
-      <MaxWidthWrapper className="snap-start">
-        <TopNavbar />
-        <HeroSection />
-      </MaxWidthWrapper>
-      <div className="flex w-full snap-start" id="work">
+    <main className="flex min-h-screen flex-col w-full bg-zinc-100 snap-y snap-proximity">
+      <div className="flex w-full min-h-screen snap-start">
+        <MaxWidthWrapper className="snap-start min-h-screen">
+          <TopNavbar />
+          <HeroSection />
+        </MaxWidthWrapper>
+      </div>
+      <div className="flex w-full min-h-screen  snap-start" id="work">
         <GridBackgroundDemo >
           <MaxWidthWrapper>
             <WebDevSection />
           </MaxWidthWrapper>
         </GridBackgroundDemo>
       </div>
-      <div className="flex flex-col bg-slate-950 grid-cols-1 gap-0 w-full min-h-screen items-start justify-start relative text-white snap-start pb-4" id="workTwo">
+      <div className="flex flex-col  bg-zinc-950 grid-cols-1 gap-0 w-full min-h-screen items-start justify-start relative text-white snap-start pb-4" id="workTwo">
         <LampContainer className="flex w-full h-fit relative ">
           <div className="-translate-y-12 2xl:-translate-y-6 text-2xl 2xl:text-7xl lg:text-2xl font-semibold pb-8 drop-shadow-[5px_5px_8px_#02a9ac] bg-clip-text text-transparent bg-gradient-to-b from-cyan-400 from-10% to-cyan-100 to-70% ">
             Explore some of my features
@@ -63,6 +65,15 @@ export default function Home() {
           </MaxWidthWrapper>
         </div>
         
+      </div>
+      <div className="flex w-full min-h-screen snap-start" id="workThree">
+        <GridBackgroundDemo className="dark:bg-grid-small-orange-500/[0.5] bg-grid-small-orange-500/[0.5]">
+          <MaxWidthWrapper>
+            <div className="flex inset-0 items-center justify-center text-5xl font-semibold">
+              SOON...
+            </div>
+          </MaxWidthWrapper>
+        </GridBackgroundDemo>
       </div>
     </main>
   );
--- src/components/ui/AvatarList.tsx
+++ src/components/ui/AvatarList.tsx
@@ -3,19 +3,23 @@ import Image from "next/image"
 
 
 const data = [
-    {id: 1, src:"/AVATAR5.jpg"},
-    {id: 2, src:"/avatar4.png"},
-    {id: 3, src:"/avatar3.png"},
-    {id: 4, src:"/avatar2.png"},
-    {id: 5, src:"/AVATAR6.jpg"},
+    {id: 1, src:"/AVATAR5.jpg", statut: "bg-green-500", online: true},
+    {id: 2, src:"/avatar4.png", statut: "bg-yellow-500", online: true},
+    {id: 3, src:"/avatar3.png", statut: "bg-yellow-500", online: false},
+    {id: 4, src:"/avatar2.png", statut: "bg-red-500", online: true},
+    {id: 5, src:"/AVATAR6.jpg", statut: "bg-green-500", online: true},
 ]
 
 const AvatarList = () => {
   return (
-    <div className='flex flex-row w-full items-center justify-center -space-x-6 cursor-pointer'>
+    <div className='flex flex-row w-full items-center justify-center -space-x-4 cursor-pointer'>
         {data.map((data) => (
-           <Image key={data.id} src={data.src} alt="avatar" width={100} height={100} 
-            className='w-16 h-16 border-2 border-white rounded-full hover:scale-110 hover:-translate-y-2 duration-300 transition-all ease-in-out' />
+          <div key={data.id} className='flex relative hover:scale-110 hover:-translate-y-2 duration-300 transition-all ease-in-out'>
+            <Image src={data.src} alt="avatar" width={100} height={100} 
+            className='w-16 h-16 border-2 border-white rounded-full ' />
+            <div className={`w-6 h-6 rounded-full ${data.statut} absolute -bottom-2 left-1 border-4 border-slate-950`} />
+            {!data.online && <div className={`w-3 h-3 rounded-full bg-slate-950 absolute bottom-0 left-3 border border-slate-950 `} />}
+          </div>
         ))}
         
 
