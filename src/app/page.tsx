fcdc122b817e7b217120269db05351be81b987f7
Mohamed Jalal <technochtak3@gmail.com>
2024-03-16 02:24:46 +0000

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col  bg-zinc-950 grid-cols-1 gap-0 w-full min-h-screen items-start justify-start relative text-white snap-start pb-4" id="workTwo">
         <LampContainer className="flex w-full h-fit relative ">
-          <div className="-translate-y-12 2xl:-translate-y-6 text-2xl 2xl:text-7xl lg:text-2xl font-semibold pb-8 drop-shadow-[5px_5px_8px_#02a9ac] bg-clip-text text-transparent bg-gradient-to-b from-cyan-400 from-10% to-cyan-100 to-70% ">
+          <div className="-translate-y-12 2xl:-translate-y-6 text-xl 2xl:text-7xl lg:text-2xl font-semibold pb-8 drop-shadow-[5px_5px_8px_#02a9ac] bg-clip-text text-transparent bg-gradient-to-b from-cyan-400 from-10% to-cyan-100 to-70% ">
             Explore some of my features
           </div>
           <div className="absolute top-0 bottom-0 flex w-full h-full">
