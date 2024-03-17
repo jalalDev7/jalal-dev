a6037fd767aeaa16982272d61ced194a2aa3a374
Mohamed Jalal <technochtak3@gmail.com>
2024-03-17 04:10:19 +0000

    main

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -4,8 +4,8 @@ import { Loader2 } from 'lucide-react'
 const LoadingPage = () => {
 
   return (
-    <div className='flex flex-col lg:flex-row min-w-screen min-h-screen items-center justify-center bg-zinc-100 '>
-        <Loader2 className="w-44 h-44 animate-spin" />
+    <div className='flex flex-col min-w-screen min-h-screen items-center justify-center bg-zinc-100 '>
+      <Loader2 className="w-44 h-44 animate-spin" />
     </div>
   )
 }
