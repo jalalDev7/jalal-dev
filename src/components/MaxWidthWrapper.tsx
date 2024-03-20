051f89fc2eb76adf5a66a09615e05064b7dda77e
Mohamed Jalal <technochtak3@gmail.com>
2024-03-20 20:21:29 +0000

    main

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,13 +1,7 @@
 import React, { ReactNode } from 'react'
 import { cn } from "@/lib/utils";
 
-const MaxWidthWrapper = ({
-    className,
-    children,
-}: {
-    className?: string
-    children: ReactNode
-}) => {
+const MaxWidthWrapper = ({className,children}: {className: String,children: ReactNode} ) => {
   return (
     <div className={cn('flex flex-col w-full px-4  max-w-[1500px] mx-auto relative', className)}>
         {children}
@@ -16,3 +10,5 @@ const MaxWidthWrapper = ({
 }
 
 export default MaxWidthWrapper
+
+
