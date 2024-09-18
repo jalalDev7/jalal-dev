45f2e03a669c06af10dacde9f616d2c5491b3250
Mohamed Jalal <technochtak3@gmail.com>
2024-09-18 18:04:55 +0100

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,12 @@ import { MdDarkMode } from "react-icons/md";
 import { IoSunny } from "react-icons/io5";
 import { useTheme } from "next-themes";
 import HireMeDialog from "@/components/HireMeDialog";
+import ClickMeButton from "@/components/ui/ClickMeButton";
+import AvatarList from "@/components/ui/AvatarList";
+import ProductTemplate from "@/components/ProductTemplate";
+import SliderShow from "@/components/SliderShow";
+import { LampContainer } from "@/components/ui/lamp";
+import { SparklesPreview } from "@/components/SparklesComponent";
 
 export default function Home() {
   const startDiv = useRef<HTMLDivElement>(null);
@@ -108,51 +114,49 @@ export default function Home() {
             </MaxWidthWrapper>
           </GridBackgroundDemo>
         </div>
-        {/* Hidden section : Componenets  */}
-        {/* <div className="flex flex-col  bg-slate-950 grid-cols-1 gap-0 w-full min-h-screen items-start justify-start relative text-white snap-start pb-4" ref={workTwotDiv} id="componentSection">
-          <LampContainer className="flex w-full h-fit relative ">
-            <div className="-translate-y-12 2xl:-translate-y-6 text-xl lg:text-5xl 2xl:text-7xl font-semibold pb-8 drop-shadow-[5px_5px_8px_#02a9ac] bg-clip-text text-transparent bg-gradient-to-b from-cyan-400 from-10% to-cyan-100 to-70% ">
-              Explore some of my features
-            </div>
-            <div className="absolute top-0 bottom-0 flex w-full h-full">
-              <SparklesPreview />
-            </div>
-          </LampContainer>
-          <div className="flex w-full -translate-y-8">
-            <MaxWidthWrapper>
-                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 w-full gap-8 transition-all duration-500 ease-in-out">
-                  <div className="flex flex-col w-full items-center justify-center  p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
-                    <div className="group cursor-pointer flex text-2xl font-semibold bg-slate-950 rounded-lg  border-blue-500 border-2 relative hover:shadow-lg hover:shadow-blue-500 transition-all duration-500 ease-in-out">
-                      <div className="z-0 absolute flex w-full h-full bg-blue-500 border-blue-500 border-2  rounded-sm scale-x-0 group-hover:scale-x-100 transition-all duration-500 ease-in-out" />
-                        <h1 className="px-8 py-2 z-10">
-                          Hover me
-                        </h1>
-                      </div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center  p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
-                    <ClickMeButton />
-                  </div>
-                  <div className="flex flex-col lg:col-span-2 2xl:col-span-1 w-full text-2xl items-center justify-center  py-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
-                    <AvatarList />
-                  </div>
-                  <div className="flex flex-col text-2xl lg:col-span-2 2xl:col-span-1 items-center justify-center ">
-                    <ProductTemplate />
-                  </div>
-                  <div className="2xl:col-span-2  hidden 2xl:flex flex-col text-2xl w-full items-center justify-center p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
-                      <SliderShow/>
-                  </div>
-                </div>
 
-            </MaxWidthWrapper>
-          </div>
-          
-        </div> */}
         <div
           className="flex w-full min-h-screen snap-start"
           ref={workTwotDiv}
           id="workThree"
         >
           <Projects />
+        </div>
+
+        <div
+          className="flex flex-col  bg-slate-950 grid-cols-1 gap-0 w-full min-h-screen items-start justify-start relative text-white snap-start pb-4"
+          ref={workTwotDiv}
+          id="componentSection"
+        >
+          <LampContainer className="flex w-full h-fit relative ">
+            <div className="-translate-y-12 2xl:-translate-y-6 text-xl lg:text-5xl 2xl:text-7xl font-semibold pb-8 drop-shadow-[5px_5px_8px_#02a9ac] bg-clip-text text-transparent bg-gradient-to-b from-cyan-400 from-10% to-cyan-100 to-70% ">
+              Custom-Built Interface Elements
+            </div>
+          </LampContainer>
+          <div className="flex w-full -translate-y-8">
+            <MaxWidthWrapper>
+              <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 w-full gap-8 transition-all duration-500 ease-in-out">
+                <div className="flex flex-col w-full items-center justify-center  p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
+                  <div className="group cursor-pointer flex text-2xl font-semibold bg-slate-950 rounded-lg  border-blue-500 border-2 relative hover:shadow-lg hover:shadow-blue-500 transition-all duration-500 ease-in-out">
+                    <div className="z-0 absolute flex w-full h-full bg-blue-500 border-blue-500 border-2  rounded-sm scale-x-0 group-hover:scale-x-100 transition-all duration-500 ease-in-out" />
+                    <h1 className="px-8 py-2 z-10">Hover me</h1>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center justify-center  p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
+                  <ClickMeButton />
+                </div>
+                <div className="flex flex-col lg:col-span-2 2xl:col-span-1 w-full text-2xl items-center justify-center  py-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
+                  <AvatarList />
+                </div>
+                <div className="flex flex-col text-2xl lg:col-span-2 2xl:col-span-1 items-center justify-center ">
+                  <ProductTemplate />
+                </div>
+                <div className="2xl:col-span-2  hidden 2xl:flex flex-col text-2xl w-full items-center justify-center p-8 rounded-2xl border-2 border-cyan-500/60  hover:shadow-xl hover:shadow-cyan-500/20 transition-all duration-500 ease-in-out">
+                  <SliderShow />
+                </div>
+              </div>
+            </MaxWidthWrapper>
+          </div>
         </div>
         <div className="flex w-full min-h-screen  snap-start">
           <GridBackgroundDemo>
--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -3,50 +3,68 @@ import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-
 export const LampContainer = ({
-    children,
-    className,
-  }: {
-    children: React.ReactNode;
-    className?: string;
-  }) => {
-    return (
-      <div className={cn("relative flex h-full flex-col items-center justify-start overflow-clip bg-slate-950 w-full rounded-md z-0",className)}>
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "relative flex h-full flex-col items-center justify-start  bg-slate-950 w-full rounded-md z-0",
+        className
+      )}
+    >
+      <div className="relative flex w-full flex-1 2xl:scale-y-125 scale-y-100 items-start py-12 justify-center isolate z-0 ">
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "80%" }}
+          transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+        >
+          <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+        </motion.div>
 
-        <div className="relative flex w-full flex-1 2xl:scale-y-125 scale-y-100 items-start py-12 justify-center isolate z-0 ">
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "80%" }}
+          transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto left-1/2 h-56 w-[100%] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
+        >
+          <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+        </motion.div>
 
-          <motion.div initial={{ opacity: 0.5, width: "15rem" }} whileInView={{ opacity: 1, width: "80%" }} transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut", }}
-            style={{ backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`, }}
-            className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]" >
-            <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-            <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-          </motion.div>
+        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md" />
+        <div className="absolute inset-auto z-50 h-36 w-[80%] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl" />
 
-          <motion.div initial={{ opacity: 0.5, width: "15rem" }} whileInView={{ opacity: 1, width: "80%" }} transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut"}}
-            style={{ backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`, }}
-            className="absolute inset-auto left-1/2 h-56 w-[100%] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]" >
-            <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-            <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          </motion.div>
+        <motion.div
+          initial={{ width: "8rem" }}
+          whileInView={{ width: "16rem" }}
+          transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
+        />
 
+        <motion.div
+          initial={{ width: "15rem" }}
+          whileInView={{ width: "80%" }}
+          transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+          className="absolute inset-auto z-50 h-0.5 w-[80%]  bg-cyan-400 "
+        />
 
-          <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md" />
-          <div className="absolute inset-auto z-50 h-36 w-[80%] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl" />
-
-          <motion.div initial={{ width: "8rem" }}  whileInView={{ width: "16rem" }} transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut", }}
-            className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl" />
-
-          <motion.div initial={{ width: "15rem" }} whileInView={{ width: "80%" }} transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut", }}
-            className="absolute inset-auto z-50 h-0.5 w-[80%]  bg-cyan-400 "/>
-   
-          
-          <div className="relative z-50 flex h-52 w-full flex-col items-center justify-end px-5 ">
-            {children}
-          </div>
+        <div className="relative z-50 flex h-52 w-full flex-col items-center justify-end px-5 ">
+          {children}
         </div>
-        
-        
       </div>
-    );
-  };
+    </div>
+  );
+};
